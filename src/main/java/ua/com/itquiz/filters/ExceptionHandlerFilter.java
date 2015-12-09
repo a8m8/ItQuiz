9d0056468d8b85296c639a23f35b0565d77a8118
Artur <Artur.Meshcheriakov@gmail.com>
2015-12-09 21:32:48 +0200

    add global exception filter

---
+++ src/main/java/ua/com/itquiz/filters/ExceptionHandlerFilter.java
@@ -0,0 +1,41 @@
+package ua.com.itquiz.filters;
+
+import org.apache.log4j.Logger;
+import ua.com.itquiz.exceptions.InvalidUserInputException;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Artur Meshcheriakov
+ */
+public class ExceptionHandlerFilter implements Filter {
+
+    private final static Logger LOGGER = Logger.getLogger(ExceptionHandlerFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        try {
+            chain.doFilter(request, response);
+        } catch (Exception ex) {
+            Throwable causeException = ex.getCause() == null ? ex : ex.getCause();
+            if (!(causeException instanceof InvalidUserInputException)) {
+                LOGGER.error(ex.getClass(), ex);
+            } else {
+                LOGGER.warn("Unsupported exception: " + ex.getClass(), ex);
+            }
+            ((HttpServletResponse) response).sendRedirect("error");
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
