eacd1e4de05d11cca17a338babd2f04727aa77d0
Artur <Artur.Meshcheriakov@gmail.com>
2016-01-04 15:52:14 +0200

    add scheduler

--- src/main/java/net/itquiz/services/impl/SchedulerService.java
+++ src/main/java/net/itquiz/services/impl/SchedulerService.java
@@ -2,8 +2,6 @@ package net.itquiz.services.impl;
 
 import net.itquiz.dao.AccountDao;
 import net.itquiz.entities.Account;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -21,19 +19,15 @@ import java.util.List;
 @Transactional
 public class SchedulerService {
 
-    protected final Logger LOGGER = LoggerFactory.getLogger(getClass());
-
     @Autowired
     private AccountDao accountDao;
 
     @Scheduled(cron = "0 0 * * * *")
-    public void testScheduler() {
+    public void deleteUnconfirmedAccounts() {
         if (accountDao.countUnconfirmedAccounts() > 0) {
             List<Account> accounts = accountDao.getAllUnconfirmed();
             for (Account account : accounts) {
                 if (System.currentTimeMillis() - account.getCreated().getTime() > 86400000) {
-                    LOGGER.error("I will delete account with email " + account.getEmail() + " created " +
-                            account.getCreated());
                     accountDao.delete(account);
                 }
             }
