f85abdfe87a78a9a484213756ccc4dcb6abcdbbd
Artur <Artur.Meshcheriakov@gmail.com>
2016-01-12 02:48:03 +0200

    change the spring security policy

---
+++ src/main/java/net/itquiz/components/impl/DefaultAccessDeniedHandler.java
@@ -0,0 +1,21 @@
+package net.itquiz.components.impl;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandlerImpl;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Artur Meshcheriakov
+ */
+@Component
+public class DefaultAccessDeniedHandler extends AccessDeniedHandlerImpl {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.sendRedirect("/login");
+    }
+}
